fa943a2874ccb529bd21566a6e4d8f5d0e7629bd
GameZebra <mr.xmitkotox@gmail.com>
2024-03-13 16:52:01 +0200

    took the data from task 2 and made it automated

--- lab_4_5/task_2/my_hist.m
+++ lab_4_5/task_2/my_hist.m
@@ -2,10 +2,24 @@ s = tf('s');
 W = (0.62/1.2)*2/((6.8*0.62*s+1)*(3.3*2.4*s+1));
 T0 = 1;
 Wd = c2d(W, T0);
+const = [0, 0.1, 0.25];
+names = ['c1.txt'; 'c2.txt';'c3.txt'];
 
-% run the simulink first
-time = out.tout;
-y = out.simout.signals.values(:,1);
-u = out.simout.signals.values(:,2);
-figure(1), hist(u)
-figure(2), histfit(y)
+for i = 1:length(const)
+    c = const(i);
+    sim("random_signal.slx");
+    time = ans.tout;
+    y = ans.simout.signals.values(:,1);
+    u = ans.simout.signals.values(:,2);
+    fid = fopen(names(i,:), 'w');
+    for j = 1:500
+        fprintf(fid, '%f\t%f\t%f\n', time(j), y(j), u(j));
+    end
+    fclose(fid);
+    figure(1);
+    subplot(3,2,2*i-1), histfit(u);
+    subplot(3,2,2*i), histfit(y);
+end
+
+
+
